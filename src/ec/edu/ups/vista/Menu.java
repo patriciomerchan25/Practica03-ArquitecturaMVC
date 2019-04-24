ee719e8ad785a5a602b3dcd9ff3edb5057a24156
Patricio Merchan <wilmermerchan1@gmail.com>
2019-04-23 19:04:14 -0500

    Creacion de paquete Controladores y  1 clase Controlador por cada clase hija Creacion de paquete Vista y 1 clase menu

---
+++ src/ec/edu/ups/vista/Menu.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ec.edu.ups.vista;
+
+/**
+ *
+ * @author patrick
+ */
+public class Menu {
+    
+    public static void main(String[] args) {
+        
+    }
+}
