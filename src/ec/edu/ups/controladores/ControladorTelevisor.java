a65673f139ea83b095593bb5030c4f0b7a576f3d
Patricio Merchan <wilmermerchan1@gmail.com>
2019-04-23 19:22:26 -0500

    Implementado la Interfaz Set y la clase HashSet en el controlador AireAcondicionado con el Crud.

--- src/ec/edu/ups/controladores/ControladorTelevisor.java
+++ src/ec/edu/ups/controladores/ControladorTelevisor.java
@@ -4,6 +4,11 @@
  * and open the template in the editor.
  */
 package ec.edu.ups.controladores;
+import ec.edu.ups.clases.Televisor;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 /**
  *
@@ -11,4 +16,49 @@ package ec.edu.ups.controladores;
  */
 public class ControladorTelevisor {
     
+    private Set<Televisor> lista;
+    private int codigo; 
+
+    public ControladorTelevisor() {
+        lista = new HashSet<>();
+        codigo = 0;
+    }
+    
+    public void crear(Televisor objeto){
+        
+        objeto.setCodigo(codigo);
+        codigo++;
+        lista.add(objeto);        
+    }
+    
+    public Televisor read(int codigo){
+        for (Televisor televisor : lista) {
+            if(televisor.getCodigo() == codigo){
+                return televisor;
+            }
+        }        
+        return null;
+    }
+    
+    public void update(Televisor objeto){
+        if(lista.contains(objeto)){
+            lista.remove(objeto);
+            lista.add(objeto);
+        }
+    }
+    
+    public void delete(int codigo){
+        for (Televisor televisor : lista) {
+            if(televisor.getCodigo() == codigo){
+                lista.remove(televisor);
+                break;
+            }
+        }  
+    } 
+    public void imprimir(){
+        for (Televisor televisor : lista) {
+            System.out.println(televisor.getNombre());   
+        }
+    }
 }
+
