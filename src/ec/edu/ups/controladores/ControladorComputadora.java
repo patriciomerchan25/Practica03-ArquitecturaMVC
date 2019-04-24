a204f5a8d756d3af74457c17966d42c873c6e473
Patricio Merchan <wilmermerchan1@gmail.com>
2019-04-23 19:33:52 -0500

    Implementado la Interfaz Map y la clase TreeMap en el controlador Computadora con el Crud.

--- src/ec/edu/ups/controladores/ControladorComputadora.java
+++ src/ec/edu/ups/controladores/ControladorComputadora.java
@@ -4,11 +4,44 @@
  * and open the template in the editor.
  */
 package ec.edu.ups.controladores;
-
+import ec.edu.ups.clases.ComputadoraPersonal;
+import java.util.Map;
+import java.util.TreeMap;
 /**
  *
  * @author patrick
  */
+
 public class ControladorComputadora {
+      private Map<Integer,ComputadoraPersonal> lista;
+      private int codigo;
+    public ControladorComputadora() {
+        lista = new TreeMap<>();
+        codigo = 0;
+    }
+    
+    public void create(ComputadoraPersonal objeto){
+        codigo ++;
+        objeto.setCodigo(codigo);
+        lista.put(codigo, objeto);
+    }
+    
+    public ComputadoraPersonal read(int codigo){
+          if(lista.get(codigo) != null){
+                return lista.get(codigo);
+            }
+          return null;
+    }
+    
+    public void update(ComputadoraPersonal objeto){
+        lista.put(objeto.getCodigo(), objeto);
+    }
+    
+    public void delete(int codigo){
+        lista.remove(codigo);
+}
+      public void imprimir(){
+        System.out.println(lista.entrySet());
+    }
     
 }
